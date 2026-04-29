9a3aba0bb54a1826ce6a5bac0d699640d8234840
ichynul <ichynul@163.com>
2026-04-29 18:23:13 +0800

    vexip v2.3.39

--- src/common/Table.php
+++ src/common/Table.php
@@ -716,8 +716,16 @@ class Table extends TWrapper implements Renderable
                 __ids__ : ids,
                 __columns__ : {$table}UseChooseColumns.value.join(','),
             },
+            {$search}Data,
             {$table}Sort ? { __sort__ : {$table}Sort } : null,
         );
+
+        params = Object.keys(params)
+            .filter(key => !/^__button\d+$/.test(key) && key != 'search_buttons')
+            .reduce((acc, key) => {
+                acc[key] = params[key];
+                return acc;
+            }, {});
 
         params = {$search}Convert(params);
 
@@ -1288,7 +1296,6 @@ EOT;
 
     /**
      * Undocumented function
-     * @param string $name
      * 
      * @return FieldsContent
      */
